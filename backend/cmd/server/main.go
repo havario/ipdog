8cd522e1815b1fb476351cffec819eedab0b1ade
havario <157877551+havario@users.noreply.github.com>
2025-12-13 02:25:19 +0800

    backend init
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/honeok/ipdog/backend/api"
+)
+
+func main() {
+	api.Run()
+}
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/honeok/ipdog/backend
+
+go 1.25.5
+
+require github.com/jinzhu/now v1.1.5
